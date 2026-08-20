1ee21cf964244ad584c450bac47644eb88f50530
Egor <erobson@MacBook-Air-Egor.local>
2026-08-20 16:29:54 +0300

    Removed some files

--- docs/html/files_dup.js
+++
@@ -1,4 +0,0 @@
-var files_dup =
-[
-    [ "main.cc", "main_8cc.html", "main_8cc" ]
-];
--- docs/html/search/all_2.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['solvelinear_0',['SolveLinear',['../main_8cc.html#ac1ba2f9d576c716484869414f91b7151',1,'main.cc']]],
-  ['solvequadratic_1',['SolveQuadratic',['../main_8cc.html#ac1dcbe4eb26d3fc8d42250855dbf56bb',1,'main.cc']]],
-  ['solver_5finf_5froots_2',['SOLVER_INF_ROOTS',['../main_8cc.html#aaa4fb24a17ce56e173a77a592bbefb1f',1,'main.cc']]]
-];
