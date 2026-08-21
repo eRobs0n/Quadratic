25c75e1163aab88f68189b8927bcb80052e79077
Egor <erobson@MacBook-Air-Egor.local>
2026-08-21 15:29:40 +0300

    Improved input

--- main.cc
+++ main.cc
@@ -1,3 +1,5 @@
+#include <cctype>
+#include <cmath>
 #include <cstdio>
 #include <stdio.h>
 #include <math.h>
@@ -6,75 +8,140 @@
 #include <string.h>
 #include <ctype.h>
 
-#define SOLVER_INF_ROOTS -1
-#define EPS 1e-9
-#define MAX_INPUT_LENGTH 100
+const double EPS = 1e-9;
+
+bool IsZero(double d){
+	return fabs(d) < EPS;
+}
+
+bool CheckDouble(double d){
+	return !isnan(d) && isfinite(d);
+}
+
+enum InputStatus{
+	VALID_INPUT,
+	INVALID_STR,
+	INVALID_NUM
+};
+
+enum EquationRootsNumber{
+	NO_ROOTS,
+	ONE_ROOT,
+	TWO_ROOTS,
+	INF_ROOTS
+};
 
 /**
- * @param [in] a a-coefficient
- * @param [in] b b-coefficient
- * @param [out] x Pointer to the root
+ * @param [in] coeff_of_x coefficient of x
+ * @param [in] free_coeff free coefficient
+ * @param [out] root Pointer to the root
  * @return Number of roots
  * @note In case of infinit number of roots returns SOLVER_INF_ROOTS
 */
 
-int SolveLinear(double a, double b, double* x){
-	assert(isfinite(a));
-	assert(isfinite(b));
-	assert(x != NULL);
+int SolveLinear(double coeff_of_x, double free_coeff, double* root){
+	assert(isfinite(coeff_of_x));
+	assert(isfinite(free_coeff));
+	assert(root != NULL);
 
-	if (abs(a) < EPS)
-		return (abs(b) < EPS) ? SOLVER_INF_ROOTS : 0;
+	if (IsZero(coeff_of_x)){
+		return (IsZero(free_coeff)) ? INF_ROOTS : 0;
+	}
 	else{
-		*x = -b/a;
+		*root = -free_coeff/coeff_of_x;
 		return 1;
 	}
 }
 
 /**
- * @param [in] a a-coefficient
- * @param [in] b b-coefficient
- * @param [in] c c-coefficient
- * @param [out] x1 Pointer to first root
- * @param [out] x2 Pointer to second root
+ * @param [in] coeff_of_square_x coefficient of x^2
+ * @param [in] coeff_of_x coefficient of x
+ * @param [in] free_coeff free coefficeint
+ * @param [out] root1 Pointer to first root
+ * @param [out] root2 Pointer to second root
  * @return Number of roots
- * @note In case of infinit number of roots returns SOLVER_INF_ROOTS
+ * @note In case of infinit number of roots returns INF_ROOTS
 */
 
-int SolveQuadratic(double a, double b, double c, double* x1, double* x2){
-	assert(isfinite(a));
-	assert(isfinite(b));
-	assert(x1 != NULL);
-	assert(x2 != NULL);
-	assert(x1 != x2);
+int SolveQuadratic(double coeff_of_square_x, double coeff_of_x, double free_coeff, double* root1, double* root2){
+	assert(isfinite(coeff_of_square_x));
+	assert(isfinite(coeff_of_x));
+	assert(root1 != NULL);
+	assert(root2 != NULL);
+	assert(root1 != root2);
 
-	if (abs(a) < EPS){
-		return SolveLinear(b, c, x1);
+	if (IsZero(coeff_of_square_x)){
+		return SolveLinear(coeff_of_x, free_coeff, root1);
 	}
 	else{
-		double d = b*b - 4*a*c;
-		if (fabs(d) < EPS){
-			*x1 = -b / (2*a);
+		double d = coeff_of_x*coeff_of_x - 4*coeff_of_square_x*free_coeff;
+		if (IsZero(d)){
+			*root1 = -coeff_of_x / (2*coeff_of_square_x);
 			return 1;
 		}
 		else if (d < 0){
 			return 0;
 		}
 		else{
-			*x1 = (-b + sqrt(d)) / (2 * a);
-			*x2 = (-b - sqrt(d)) / (2 * a);
+			double sqrt_d = sqrt(d);
+			*root1 = (-coeff_of_x + sqrt_d) / (2 * coeff_of_square_x);
+			*root2 = (-coeff_of_x - sqrt_d) / (2 * coeff_of_square_x);
 			return 2;
 		}
 	}
 }
 
 int CheckStringOfDigits(char *s){
+	assert(s != NULL);
+
 	char c;
 	while ((c = *s++) != '\0'){
 		if (!isdigit(c))
 			return 0;
 	}
 	return 1;
+}
+
+void ClearStdinBuffer(){
+	int c = -1;
+	while ((c = getchar()) != '\n' && c != EOF);
+}
+
+enum InputStatus EnterCoefficients(double* a, double* b, double* c){
+	assert(a != NULL);
+	assert(b != NULL);
+	assert(c != NULL);
+
+	char sep1 = 0, sep2 = 0, sep3 = 0;
+	int argument_cnt = scanf("%lf%c%lf%c%lf%c", a, &sep1, b, &sep2, c, &sep3);
+	if (argument_cnt != 6 || sep1 != ' ' || sep2 != ' ' || sep3 != '\n'){
+		return INVALID_STR;
+	}
+	if (!CheckDouble(*a) || !CheckDouble(*b) || !CheckDouble(*c)){
+		return INVALID_NUM;
+	}
+	return VALID_INPUT;
+}
+
+void PrintRoots(int nRoots, double x1, double x2, int digits_after_point){
+	switch (nRoots){
+		case NO_ROOTS:
+			printf("No roots\n");
+			break;
+		case INF_ROOTS:
+			printf("Infinity number of roots\n");
+			break;
+		case ONE_ROOT:
+			printf("x = %.*lf\n", digits_after_point, x1);
+			break;
+		case TWO_ROOTS:
+			printf("x1 = %.*lf, x2 = %.*lf\n", digits_after_point, x1, digits_after_point, x2);
+			break;
+		default:
+			printf("Something went wrong...\n");
+			break;
+	}
+	
 }
 
 int main(int argc, char* argv[]){
@@ -92,36 +159,33 @@ int main(int argc, char* argv[]){
 	}
 
 	printf("Quadratic Equation Solver\n ax\u00B2 + bx + c = 0\n"); //\u00B2 - unicode ² symbol
-	printf("Enter coefficients a, b, c: ");
 
-	char trash_symbols;
-	char user_input[MAX_INPUT_LENGTH];
-	fgets(user_input, MAX_INPUT_LENGTH, stdin);
-	int input_param_count = sscanf(user_input, "%lf %lf %lf %c", &a, &b, &c, &trash_symbols);
+	printf("Enter coefficients: ");
 
-	while (input_param_count != 3){
-		printf("Incorrect input\nEnter coefficients a, b, c: ");
-		a = 0., b = 0., c = 0.;
-		fgets(user_input, MAX_INPUT_LENGTH, stdin);
-		input_param_count = sscanf(user_input, "%lf %lf %lf %c", &a, &b, &c, &trash_symbols);
+	enum InputStatus input_result = EnterCoefficients(&a, &b, &c);
+	while (input_result != VALID_INPUT){
+		if (input_result == INVALID_NUM){
+			printf("Invalid numbers. Don't use nan or inf\nEnter coefficients: ");
+		}else if(input_result == INVALID_STR){
+			printf("Invalid input. Please enter 3 numbers separated by space\nEnter coefficients: ");
+			ClearStdinBuffer();
+		}else{
+			printf("Please try again.\n");
+		}
+		input_result = EnterCoefficients(&a, &b, &c);
 	}
 
-	printf("You entered %.*lfx\u00B2 %c %.*lfx %c %.*lf = 0 equation.\n", digits_after_point,
-	a, (b>0) ? '+' : '-', digits_after_point, fabs(b), (c>0) ? '+' : '-', digits_after_point, fabs(c)); 
+	printf("You entered %.*lfx\u00B2 %c %.*lfx %c %.*lf = 0 equation.\n", 
+		                     digits_after_point,      a, 
+		(b>=0) ? '+' : '-', digits_after_point, fabs(b), 
+		(c>=0) ? '+' : '-', digits_after_point, fabs(c)
+		); 
+
 
 	double x1 = 0., x2 = 0.;
 	int nRoots = SolveQuadratic(a, b, c, &x1, &x2);
 
-	if (nRoots == 0) 
-		printf("No roots\n");
-	else if (nRoots == SOLVER_INF_ROOTS)
-		printf("Infinity number of roots\n");
-	else if(nRoots == 1)
-		printf("x = %.*lf\n", digits_after_point, x1);
-	else if (nRoots == 2)
-		printf("x1 = %.*lf, x2 = %.*lf\n", digits_after_point, x1, digits_after_point, x2);
-	else
-		printf("Something went wrong...\n");
+	PrintRoots(nRoots, x1, x2, digits_after_point);
 
 	return 0;
 }
