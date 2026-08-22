0c28084a0bb3bc621ee83d9469d26b0ae6ec3b45
Egor <erobson@MacBook-Air-Egor.local>
2026-08-22 14:43:45 +0300

    Testing

--- main.cc
+++ main.cc
@@ -15,6 +15,13 @@ const double EPS = 1e-9;
 const int COLOR_STR_MAX_LEN = 100;
 const char* CONSOLE_RESET = "\033[0m\033[K";
 
+const int DEFAULT_ACCURACY = 2;
+const int DEFAULT_TEST_COUNT = 10;
+const bool DEFAULT_NEED_TEST = false;
+
+const int RND_TEST_UPPER = 10;
+const int RND_TEST_LOWER = -10;
+
 enum ConsoleColors{
 	NO_COLOR = -1,
 	BLACK,
@@ -27,6 +34,24 @@ enum ConsoleColors{
 	WHITE
 };
 
+enum EquationSolutionsNumber{
+	NO_ROOTS,
+	ONE_ROOT,
+	TWO_ROOTS,
+	INF_ROOTS
+};
+
+enum InputStatus{
+	VALID_INPUT,
+	INVALID_STR,
+	INVALID_NUM
+};
+
+enum TestStatus{
+	TEST_FAILED, 
+	TEST_PASSED
+};
+
 struct EquationCoeffs{
 	double coeff_of_sq_x;
 	double coeff_of_x;
@@ -35,7 +60,20 @@ struct EquationCoeffs{
 struct EquationSolutions{
 	double root1;
 	double root2;
+	EquationSolutionsNumber nRoots;
 };
+
+bool Equals(double a, double b){
+	return fabs(a-b) < EPS;
+}
+
+bool CompareSolutions(struct EquationSolutions a, struct EquationSolutions b){
+	if (a.nRoots != b.nRoots) return false;
+	if (a.nRoots == 1) return Equals(a.root1, b.root1);
+	if (a.nRoots == 2) return (Equals(a.root1, b.root1) && Equals(a.root2, b.root2)) || 
+							  (Equals(a.root1, b.root2) && Equals(a.root2, b.root1));
+	return true;
+}
 
 void CreateColorStr(char* res_color, size_t res_color_len, enum ConsoleColors text_color, enum ConsoleColors bg_color){
 	if (text_color != NO_COLOR && bg_color != NO_COLOR) {
@@ -61,29 +99,12 @@ void ColoredPrintf(enum ConsoleColors text_color, enum ConsoleColors bg_color, c
 }
 
 bool IsZero(double d){
-	return fabs(d) < EPS;
-}
-
-bool Equals(double a, double b){
-	return fabs(a-b) < EPS;
+	return Equals(d, 0.);
 }
 
 bool CheckDouble(double d){
 	return !isnan(d) && isfinite(d);
 }
-
-enum InputStatus{
-	VALID_INPUT,
-	INVALID_STR,
-	INVALID_NUM
-};
-
-enum EquationSolutionsNumber{
-	NO_ROOTS,
-	ONE_ROOT,
-	TWO_ROOTS,
-	INF_ROOTS
-};
 
 /**
  * @param [in] coeff Array of equation coefficients
@@ -92,13 +113,13 @@ enum EquationSolutionsNumber{
  * @note In case of infinit number of roots returns INF_ROOTS
 */
 
-int SolveLinear(struct EquationCoeffs coeffs, struct EquationSolutions* roots){
+void SolveLinear(struct EquationCoeffs coeffs, struct EquationSolutions* roots){
 	if (IsZero(coeffs.coeff_of_x)){
-		return (IsZero(coeffs.free_coeff)) ? INF_ROOTS : 0;
+		roots->nRoots = (IsZero(coeffs.free_coeff)) ? INF_ROOTS : NO_ROOTS;
 	}
 	else{
 		roots->root1 = -coeffs.free_coeff/coeffs.coeff_of_x;
-		return 1;
+		roots->nRoots = ONE_ROOT;
 	}
 }
 
@@ -110,34 +131,34 @@ int SolveLinear(struct EquationCoeffs coeffs, struct EquationSolutions* roots){
  * @note In case of infinit number of roots returns INF_ROOTS
 */
 
-int SolveQuadratic(struct EquationCoeffs coeffs, struct EquationSolutions* roots){
+void SolveQuadratic(struct EquationCoeffs coeffs, struct EquationSolutions* roots){
 	double d = coeffs.coeff_of_x*coeffs.coeff_of_x - 4*coeffs.coeff_of_sq_x*coeffs.free_coeff;
 	if (IsZero(d)){
 		roots->root1 = -coeffs.coeff_of_x / (2*coeffs.coeff_of_sq_x);
-		return ONE_ROOT;
+		roots->nRoots =  ONE_ROOT;
 	}
 	else if (d < 0){
-		return NO_ROOTS;
+		roots->nRoots =  NO_ROOTS;
 	}
 	else{
 		double sqrt_d = sqrt(d);
 		roots->root1 = (-coeffs.coeff_of_x + sqrt_d) / (2 * coeffs.coeff_of_sq_x);
 		roots->root2 = (-coeffs.coeff_of_x - sqrt_d) / (2 * coeffs.coeff_of_sq_x);
-		return TWO_ROOTS;
+		roots->nRoots = TWO_ROOTS;
 	}
 }
 
 
-int SolveEquation(struct EquationCoeffs coeffs, struct EquationSolutions* roots){
+void SolveEquation(struct EquationCoeffs coeffs, struct EquationSolutions* roots){
 	assert(isfinite(coeffs.coeff_of_sq_x));
 	assert(isfinite(coeffs.coeff_of_x));
 	assert(isfinite(coeffs.free_coeff));
 	assert(roots != NULL);
 
 	if (!IsZero(coeffs.coeff_of_sq_x)){
-		return SolveQuadratic(coeffs, roots);
+		SolveQuadratic(coeffs, roots);
 	}else{
-		return SolveLinear(coeffs, roots);
+		SolveLinear(coeffs, roots);
 	}
 }
 
@@ -157,8 +178,8 @@ void ClearStdinBuffer(){
 	while ((c = getchar()) != '\n' && c != EOF);
 }
 
-void PrintRoots(int nRoots, struct EquationSolutions roots, int digits_after_point){
-	switch (nRoots){
+void PrintRoots(struct EquationSolutions roots, int accuracy){
+	switch (roots.nRoots){
 		case NO_ROOTS:
 			printf("No roots\n");
 			break;
@@ -166,10 +187,10 @@ void PrintRoots(int nRoots, struct EquationSolutions roots, int digits_after_poi
 			printf("Infinity number of roots\n");
 			break;
 		case ONE_ROOT:
-			printf("x = %.*lf\n", digits_after_point, roots.root1);
+			printf("x = %.*lf\n", accuracy, roots.root1);
 			break;
 		case TWO_ROOTS:
-			printf("x1 = %.*lf, x2 = %.*lf\n", digits_after_point, roots.root1, digits_after_point, roots.root2);
+			printf("x1 = %.*lf, x2 = %.*lf\n", accuracy, roots.root1, accuracy, roots.root2);
 			break;
 		default:
 			printf("Something went wrong...\n");
@@ -178,12 +199,19 @@ void PrintRoots(int nRoots, struct EquationSolutions roots, int digits_after_poi
 	
 }
 
-int ProcessFlags(int argc, char* argv[], int* digits_after_point){
-	if (argc>1){
-		if (strcmp(argv[1], "-accuracy") == 0 && CheckStringOfDigits(argv[2])){ // maybe accuracy
-			*digits_after_point = atoi(argv[2]);
+int ProcessFlags(int argc, char* argv[], int* accuracy, bool* need_test){
+	assert(accuracy != NULL);
+
+	for (int i = 1; i < argc; i++){
+		if (strcmp(argv[i], "--accuracy") == 0 && i+1 < argc && CheckStringOfDigits(argv[i+1])){
+			*accuracy = atoi(argv[i+1]);
+			i+=1;
+		}
+		else if (strcmp(argv[i], "--test") == 0){ 
+			*need_test = true;
 		}else{
-			ColoredPrintf(WHITE, RED, "Usage: qdr -accuracy digits_after_point\n");
+			//printf("%s\n", argv[i]);
+			ColoredPrintf(WHITE, RED, "Usage: qdr --accuracy *accuracy* --test\n");
 			return -1;
 		}
 	}
@@ -194,14 +222,15 @@ void Greeting(){
 	ColoredPrintf(CYAN, BLACK, "Quadratic Equation Solver\n ax\u00B2 + bx + c = 0"); //\u00B2 - unicode ² symbol
 }
 
-void PrintEquation(struct EquationCoeffs coeffs, int digits_after_point){
-	printf("You entered ");
-	ColoredPrintf(GREEN, BLACK, "%.*lf", digits_after_point, coeffs.coeff_of_sq_x);
+void PrintEquation(struct EquationCoeffs coeffs, int accuracy){
+	//printf("You entered ");
+	ColoredPrintf(GREEN, BLACK, "%.*lf", accuracy, coeffs.coeff_of_sq_x);
 	printf("x\u00B2%c", (coeffs.coeff_of_x>=0) ? '+' : ' ');
-	ColoredPrintf(GREEN, BLACK, "%.*lf", digits_after_point, coeffs.coeff_of_x);
+	ColoredPrintf(GREEN, BLACK, "%.*lf", accuracy, coeffs.coeff_of_x);
 	printf("x%c", (coeffs.free_coeff>=0) ? '+' : ' ');
-	ColoredPrintf(GREEN, BLACK, "%.*lf", digits_after_point, coeffs.free_coeff);
-	printf(" = 0 equation.\n");
+	ColoredPrintf(GREEN, BLACK, "%.*lf", accuracy, coeffs.free_coeff);
+	printf(" = 0\n");
+	//printf(" = 0 equation.\n");
 }
 
 enum InputStatus EnterCoefficients(struct EquationCoeffs* coeffs){
@@ -219,6 +248,8 @@ enum InputStatus EnterCoefficients(struct EquationCoeffs* coeffs){
 }
 
 void RequestCoefficients(struct EquationCoeffs* coeffs){
+	assert(coeffs != NULL);
+
 	printf("\nEnter coefficients: ");
 	enum InputStatus input_result = EnterCoefficients(coeffs);
 	while (input_result != VALID_INPUT){
@@ -236,25 +267,85 @@ void RequestCoefficients(struct EquationCoeffs* coeffs){
 	}
 }
 
+void PrintTestError(struct EquationCoeffs coeffs, struct EquationSolutions expected, struct EquationSolutions got){
+	ColoredPrintf(RED, NO_COLOR, "Test FAILED:\n");
+		PrintEquation(coeffs, 2);	
+		ColoredPrintf(RED, NO_COLOR, "expected: %lf %lf\n"
+									 "got:      %lf %lf\n", expected.root1, expected.root2,
+			got.root1, got.root2);
+}
 
+TestStatus TestSolver(struct EquationCoeffs coeffs, struct EquationSolutions rootsRef, struct EquationSolutions* test_res){
+	SolveEquation(coeffs, test_res);
+	if (!CompareSolutions(*test_res, rootsRef)){
+		return TEST_FAILED;
+	}
+	return TEST_PASSED;
+}
+
+int GetRandInBounds(int lower, int upper){
+	return lower + rand() % (upper - lower + 1);
+}
+
+struct EquationCoeffs GetCoeffsBySolution(struct EquationSolutions test_res){
+	struct EquationCoeffs test_coeffs;
+	int multiplier = GetRandInBounds(RND_TEST_LOWER, RND_TEST_UPPER);
+	test_coeffs.coeff_of_sq_x = multiplier;
+	test_coeffs.coeff_of_x = -(test_res.root1 + test_res.root2) * multiplier;
+	test_coeffs.free_coeff = multiplier * test_res.root1 * test_res.root2;
+	return test_coeffs;
+}
+
+void GenTests(size_t test_cnt, struct EquationCoeffs* test_coeffs, struct EquationSolutions* test_res){
+	assert(test_coeffs != NULL);
+	assert(test_res != NULL);
+
+	for (size_t i = 0; i < test_cnt; i++){
+		test_res[i].nRoots = TWO_ROOTS;
+
+		while (Equals(test_res[i].root1, test_res[i].root2)){
+			test_res[i].root1 = GetRandInBounds(RND_TEST_LOWER, RND_TEST_UPPER);
+			test_res[i].root2 = GetRandInBounds(RND_TEST_LOWER, RND_TEST_UPPER);
+		}
+
+		test_coeffs[i] = GetCoeffsBySolution(test_res[i]);
+		
+	}
+}
+
+void RunTests(size_t test_cnt, struct EquationCoeffs test_coeffs[], struct EquationSolutions test_res[]){
+	for (size_t i = 0; i < test_cnt; i++){
+		struct EquationSolutions result;
+		if (TestSolver(test_coeffs[i], test_res[i], &result) == TEST_FAILED) PrintTestError(test_coeffs[i], test_res[i], result);
+		else ColoredPrintf(GREEN, BLACK, "Test %d passed.\n", i+1);
+	}
+}
 
 int main(int argc, char* argv[]){
 
-	int digits_after_point = 2;
-	
-	ProcessFlags(argc, argv, &digits_after_point);
+	int accuracy = DEFAULT_ACCURACY;
+	bool need_test = DEFAULT_NEED_TEST;
+
+	ProcessFlags(argc, argv, &accuracy, &need_test);
+
+	if (need_test){
+		struct EquationCoeffs test_coeffs[DEFAULT_TEST_COUNT];
+		struct EquationSolutions test_res[DEFAULT_TEST_COUNT];
+		GenTests(DEFAULT_TEST_COUNT, test_coeffs, test_res);
+		RunTests(DEFAULT_TEST_COUNT, test_coeffs, test_res);
+	}
 
 	Greeting();
 
 	struct EquationCoeffs coeffs;
 	RequestCoefficients(&coeffs);
 
-	PrintEquation(coeffs, digits_after_point);
+	PrintEquation(coeffs, accuracy);
 
 	struct EquationSolutions roots;
-	int nRoots = SolveEquation(coeffs, &roots);
+	SolveEquation(coeffs, &roots);
 
-	PrintRoots(nRoots, roots, digits_after_point);
+	PrintRoots(roots, accuracy);
 
 	return 0;
 }
